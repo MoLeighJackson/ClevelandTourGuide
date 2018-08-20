ad506ea316b2ae7c4f01a51a02f1beb37d76fb9e
MoLeighJackson <moleighjackson@gmail.com>
2018-08-20 19:04:56 -0400

    added images

--- app/src/main/java/com/example/moleigh/clevelandtourguide/AttractionInfo.java
+++
@@ -1,4 +0,0 @@
-package com.example.moleigh.clevelandtourguide;
-
-public class AttractionInfo {
-}
--- app/src/main/java/com/example/moleigh/clevelandtourguide/EdgewaterFragment.java
+++ app/src/main/java/com/example/moleigh/clevelandtourguide/EdgewaterFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -47,6 +48,7 @@ public class EdgewaterFragment extends Fragment {
         );
 
         listView.setAdapter(listViewAdapter);
+
 
         return view;
 
